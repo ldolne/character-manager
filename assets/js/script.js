5859242404214c360127d80e91d2b7ed637fce9a
ldolne <dolnelaetitia@gmail.com>
2020-06-26 17:06:31 +0200

    Models

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const axios = require('axios').default; // avec intellisense/autocomplete
+
+(() => {
+
+
+
+
+
+
+})();
+
+
+
+const axios = require('axios').default;
+
+(async ()=>{
+    await axios.get('https://character-database.becode.xyz/characters%27).then((response)=%3E%7Bconsole.log(response)%7D);
+})();
